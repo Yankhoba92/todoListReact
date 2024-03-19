e28352d2e21654a9f52a6be0611c8bfe1aadab60
Yankhoba <bagayoko.yan@gmail.com>
2024-03-19 12:01:25 +0100

    ✨ Suppression au click

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const Todo = ({ task }) => {
+const Todo = ({ task, toggleComplete, deleteTodo}) => {
   return (
     <div className="todo">
-      <p>{task.task}</p>
+      <p onClick={() =>toggleComplete(task.id)} className={`${task.completed ? "completed" : ""}`}>{task.task}</p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faTrash} onClick={()=>deleteTodo(task.id)}/>
       </div>
     </div>
   );
