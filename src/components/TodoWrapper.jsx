0e47638a825868fcd088609d889b88efbdc1d9e1
Yankhoba <bagayoko.yan@gmail.com>
2024-03-19 11:41:03 +0100

    Nettoyage de mon code

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
-import { v4 as uuidv4 } from 'uuid'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
 uuidv4();
 
 const TodoWrapper = () => {
-  const [todos, setTodos] = useState([])
-  
-  const addTodo = (todo)=>{
-    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
-    console.log(todos);
-  }
-  return (
-    <div className='todoWrapper'>
-      <TodoForm addTodo={addTodo}/>
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
 
-export default TodoWrapper
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+  return (
+    <div className="todoWrapper">
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoWrapper;
