108c22755a0d9232d059dc16953610b33bca148a
Yankhoba <bagayoko.yan@gmail.com>
2024-03-19 11:28:27 +0100

    ✨ 🎨 add todo fonction pour l'ajout de tâches

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+  const handleChange =(e)=>{
+    e.preventDefault();
+    addTodo(value);
+    setValue("")
+  }
   return (
-    <form className='todoForm'>
-      <input type="text"
-      className='todo-Input'
-      placeholder='Tâches du Jour' />
-      <button type='submit' className='todo-btn'></button>
+    <form className="todoForm" onSubmit={handleChange}>
+      <input
+        type="text"
+        value={value}
+        className="todo-input"
+        placeholder="Tâches du Jour"
+        onChange={(e) => {
+            setValue(e.target.value);
+          }}
+      />
+      <button type="submit" className="todo-btn">
+        Tâche
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
